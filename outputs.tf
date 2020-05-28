76febef3c08a7f5a7ef13c6a861a453861bc1f8f
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-05-28 16:25:06 +0200

    [output] add appliance_back_port and show all_fixed_ips

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,15 @@
 output "appliance_front_ip" {
-  value       = openstack_networking_port_v2.appliance-management-front-port.fixed_ip
+  value       = openstack_networking_port_v2.appliance-management-front-port.all_fixed_ips
   description = "Front office IPv4 address"
 }
 
 output "appliance_back_ip" {
-  value       = openstack_networking_port_v2.appliance-management-back-port.fixed_ip
+  value       = openstack_networking_port_v2.appliance-management-back-port.all_fixed_ips
   description = "Back office IPv4 address"
+}
+
+output "appliance_back_port" {
+  value       = openstack_networking_port_v2.appliance-management-back-port
+  description = "Back office port ID"
 }
 
